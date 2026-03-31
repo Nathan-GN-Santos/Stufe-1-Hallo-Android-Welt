8813fbf40b6219a16e55cec9bfabb7ca87da9acf
Nathan-GN-Santos <nathan.g.n.santos@gmail.com>
2026-03-31 11:42:07 -0300

    fiz o botão pisca pisca commit

--- app/src/main/java/com/example/halloandroid_welt/MainActivity.kt
+++ app/src/main/java/com/example/halloandroid_welt/MainActivity.kt
@@ -14,36 +14,40 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
+        val meuBotao = findViewById<Button>(R.id.meuBotao)
+
         //variavel que muda as cores
         val listaDeCores = listOf(
+            android.graphics.Color.GRAY,
             android.graphics.Color.MAGENTA,
-            android.graphics.Color.GREEN,
+            android.graphics.Color.parseColor("#4CAF50"),
             android.graphics.Color.RED,
             android.graphics.Color.BLUE
         )
+        //variavel que muda as frases
+        val listaDeFrases = listOf(
+            "Hallo, Welt!",
+            "Das ist Magenta",
+            "Das ist Grün",
+            "Das ist Rot",
+            "Das ist Blau"
+        )
+
         //contador para saber em que cor usar
         var contadorDeCliques = 0
-        // 1. Achar o botão no XML usando o ID que demos
-        //val botaoMudarCor = findViewById<Button>(R.id.meuBotao)
 
-        // 2. Achar o texto (TextView) também
-        val meuBotao = findViewById<Button>(R.id.meuBotao) // Ajuste se o ID do seu texto for diferente
-
-        // 3. Criar a lógica do clique
-        meuBotao.setOnClickListener {
-            // Quando clicar, mude a cor do texto para VERMELHO
-            //meuBotao.setTextColor(android.graphics.Color.WHITE)
-
-            // Opcional: Mudar também a cor de fundo do PRÓPRIO BOTÃO
-            //meuBotao.setBackgroundColor(android.graphics.Color.GREEN)
-
+        fun atualizarInterface() {
             val indice = contadorDeCliques % listaDeCores.size
-            val corAtual = listaDeCores[indice]
-
-            meuBotao.setBackgroundColor(corAtual)
-
-            contadorDeCliques++
+            meuBotao.setBackgroundColor(listaDeCores[indice])
+            meuBotao.text = listaDeFrases[indice]
         }
+
+        atualizarInterface()
+        meuBotao.setOnClickListener {
+            contadorDeCliques++
+            atualizarInterface()
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
